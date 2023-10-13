f6cf9593e9b60ff6d95fcbc1111426d19ca4ce89
MaximilianStockmann <maximilian.stockmann@gmx.de>
2023-10-13 19:19:34 +0200

    working implementation

--- index.js
+++ index.js
@@ -7,17 +7,34 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
+
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 const roleInformation = "Du bist ein ChatBot, der Informationen zu Produkten im Möbelsortiment von Check24 ausgeben soll. Dir werden Informationen zu den einzelnen Produkten geschickt, die dir beim beantworten der Fragen helfen.";
 
 const data = readJSONFile(path.resolve(__dirname, "./dataset-shopping.json"));
 
-console.log("First data element: ", data[0]);
+const userQuery = "·         Produkt: product_csin 4F9B41080E0C12 -   Wie heißt die Marke?-   Wie teuer ist das Produkt?-   Passen 5 Leute gleichzeitig drauf?-   Wird mir der Esszimmertisch bis hoch in die Wohnung geliefert?";
+
+function getCsinFromString(string) {
+  const regex = /product_csin\s([A-Z0-9]+)/;
+  const match = string.match(regex);
+  
+  if (match && match[1]) {
+    const csin = match[1];
+    console.log(csin); // Here is the extracted csin
+      return csin;
+  } else {
+    console.log('csin not found in the text');
+  }
+
+
+}
 
 function getProductByCsin(csin) {
+  console.log(csin);
   const product = data.filter((element) => element.product_csin == csin);
   console.log(product)
   return product
@@ -26,8 +43,8 @@ function getProductByCsin(csin) {
 const chatCompletion = await openai.chat.completions.create({
   messages: [
     {role: "system", content: roleInformation},
-    {role: "user", content: "Hier sind einige Infos über das Produkt: " + JSON.stringify(getProductByCsin("4E0878B5768A79"))},
-    {role: "user", content: "Bitte "}
+    {role: "user", content: "Hier sind einige Infos über das Produkt: " + JSON.stringify(getProductByCsin(getCsinFromString(userQuery)))},
+    {role: "user", content: "Bitte beantworte mir folgende Fragen zum Produkt: " + userQuery}
 ],
   model: "gpt-3.5-turbo-16k",
 });
